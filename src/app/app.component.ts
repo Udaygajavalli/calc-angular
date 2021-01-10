0d8b20425bde4586b388489fc11ca752a1dbf860
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-01-10 23:24:58 +0530

    Commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calc-angular';
+  title = 'Basic Angular Calculator';
+  valueA = '';
+  valueB = '';
+  ValueAdd: any = '';
+  ValueSub: any = '';
+  ValueMul: any = '';
+  ValueDiv: any = '';
+  ValueMod: any = '';
+
+  add() {
+    this.ValueAdd = Number(this.valueA) + Number(this.valueB);
+  }
+  sub() {
+    this.ValueSub = Number(this.valueA) - Number(this.valueB);
+  }
+  mul() {
+    this.ValueMul = Number(this.valueA) * Number(this.valueB);
+  }
+  div() {
+    this.ValueDiv = Number(this.valueA) / Number(this.valueB);
+  }
+  mod() {
+    this.ValueMod = Number(this.valueA) % Number(this.valueB);
+  }
+
+  reset(){
+    this.ValueAdd = '';
+    this.ValueSub = '';
+    this.ValueDiv = '';
+    this.ValueMod = '';
+    this.ValueMul = '';
+  }
+  calc(){
+    this.add();
+    this.sub();
+    this.mul();
+    this.div();
+    this.mod();
+  }
+
 }
